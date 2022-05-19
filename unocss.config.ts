4744967f99b36f33802a0b18bd9b1b4f83891013
yixiaco <627428179@qq.com>
2022-05-19 10:17:07 +0800

    fix:更新可用版本

--- unocss.config.ts
+++ unocss.config.ts
@@ -9,10 +9,10 @@ import {
 } from 'unocss'
 
 export default defineConfig({
-  shortcuts: [
-    ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
-  ],
+  shortcuts: {
+    'icon-btn':
+        'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600',
+  },
   presets: [
     presetUno(),
     presetIcons({
